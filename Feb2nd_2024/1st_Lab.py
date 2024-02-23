97f6ea3b40908c2c67bee5a1adfc3d5ab48e6a5f
saisnehapaluvari <saisnehapaluvari@gmail.com>
2024-02-23 09:43:07 +0530

    Project1

--- Feb2nd_2024/1st_Lab.py
+++ Feb2nd_2024/1st_Lab.py
@@ -1,1 +1,38 @@
-print("great python")
+#variable1
+age = 23
+print(age)
+age = "sneha"
+print(age)
+age = 'c'
+print(age)
+
+#variable2
+_ = ("sneha")
+print(_)
+import keyword
+print(keyword.kwlist)
+print("sai","sneha","nihan")
+
+#max function
+result = max(3,4)
+print(result)
+
+#series variables
+a,b,c = 10,True,"Sneha"
+print(a,b)
+
+#type function
+print(type(a))
+
+#input function
+name = input("Enter your name")
+print("This is my name",name)
+
+#small question
+a = 10
+b = a
+c = b
+a = "Sneha"
+print(a)
+print(b)
+print(c)
---
+++ Feb2nd_2024/Task1.py
@@ -0,0 +1,6 @@
+# Take user input (name,age,roll_number,phonenumber) & print the user details
+user_name,user_age,user_roll,user_phoneNo = input("Enter the name of the user"),input("Enter the age of the user"),input("Enter the roll of the user"),input("Enter the phoneNo of the user")
+print("user details",user_name,user_age,user_roll,user_phoneNo)
+
+
+
